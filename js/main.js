32c461b550643cbea9c1356bce4e8cca2af3def2
LeshVinov <90595946+LeshVinov@users.noreply.github.com>
2022-02-27 18:19:56 +0300

    feat: section lessons done (Swiper is not working, I duno)

--- js/main.js
+++ js/main.js
@@ -8,4 +8,25 @@ $(document).ready(function () {
           prevEl: '.swiper-button-prev',
         },
       });
+
+      const secondSwiper = new Swiper('.swiper-second', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+      
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+          renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + "</span>";
+          },
+        },
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.lessons__swiper__button-next',
+          prevEl: '.lessons__swiper__button-prev',
+        },
+      });
 });
