472adbc087c87a9c6a32b4300834b3f248039f3e
LeshVinov <90595946+LeshVinov@users.noreply.github.com>
2022-03-13 16:03:53 +0300

    all done (without optimization)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
 $(document).ready(function () {
+    const menuBtn = document.querySelector('.menu-btn');
+    const menuNav = document.querySelector('.menu__nav')
+    let menuOpen = false;
+
+    menuBtn.addEventListener('click', () => {
+        if (!menuOpen) {
+            menuBtn.classList.add('open');
+            menuNav.classList.add('open');
+            menuOpen = true;
+        } else {
+            menuBtn.classList.remove('open');
+            menuNav.classList.remove('open');
+            menuOpen = false;
+        }
+    });
     const swiper = new Swiper('.swiper-benefits', {
         loop: true,
       
